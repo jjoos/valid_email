248868b06a62305400e233ff92a63db098789edb
Jan Deelstra <jan@deelstra.org>
2014-03-19 15:32:01 +0100

    Put the validation in a module method.

--- lib/valid_email/email_validator.rb
+++ lib/valid_email/email_validator.rb
@@ -3,29 +3,34 @@ require 'active_model/validations'
 require 'mail'
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record,attribute,value)
-    begin
-      return if options[:allow_nil] && value.nil?
-      return if options[:allow_blank] && value.blank?
+    return if options[:allow_nil] && value.nil?
+    return if options[:allow_blank] && value.blank?
 
-      m = Mail::Address.new(value)
-      # We must check that value contains a domain and that value is an email address
-      r = m.domain && m.address == value
-      t = m.__send__(:tree)
-      # We need to dig into treetop
-      # A valid domain must have dot_atom_text elements size > 1
-      # user@localhost is excluded
-      # treetop must respond to domain
-      # We exclude valid email values like <user@localhost.com>
-      # Hence we use m.__send__(tree).domain
-      r &&= (t.domain.dot_atom_text.elements.size > 1)
-      # Check if domain has DNS MX record
-      if r && options[:mx]
-        require 'valid_email/mx_validator'
-        r &&= MxValidator.new(:attributes => attributes).validate(record)
-      end
-    rescue Exception => e
-      r = false
+    r = ValidEmail.validate_parseable(value)
+
+    if r && options[:mx]
+      require 'valid_email/mx_validator'
+      r &&= MxValidator.new(:attributes => attributes).validate(record)
     end
+
     record.errors.add attribute, (options[:message] || I18n.t(:invalid, :scope => "valid_email.validations.email")) unless r
+  end
+end
+
+module ValidEmail
+  def self.validate_parseable(email_address)
+    m = Mail::Address.new(email_address)
+    # We must check that value contains a domain and that value is an email address
+    return false unless m.domain && m.address == email_address
+    t = m.__send__(:tree)
+    # We need to dig into treetop
+    # A valid domain must have dot_atom_text elements size > 1
+    # user@localhost is excluded
+    # treetop must respond to domain
+    # We exclude valid email values like <user@localhost.com>
+    # Hence we use m.__send__(tree).domain
+    return t.domain.dot_atom_text.elements.size > 1
+  rescue
+    false
   end
 end
